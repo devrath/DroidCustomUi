d1cd666933f3b6acecdc3712da764cf569ccc3be
Devrath AD <devrathad@mplgaming.com>
2021-03-19 21:38:16 +0530

    text

---
+++ app/src/main/java/com/demo/code/ui/custom/ErrorView.kt
@@ -0,0 +1,27 @@
+package com.demo.code.ui.custom
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.demo.code.databinding.ErrorViewBinding
+
+class ErrorView  @JvmOverloads constructor(context: Context,
+                                           attributes: AttributeSet? = null,
+                                           defStyleAttr: Int = 0
+) : ConstraintLayout(context, attributes, defStyleAttr) {
+
+    companion object {
+        const val TAG = "GameStreamErrorView"
+    }
+
+    private var binding = ErrorViewBinding.inflate(LayoutInflater.from(context), this, true)
+
+    fun hideTryAgainView() {
+        binding.txtPleaseTryAgain.visibility = View.GONE
+    }
+
+    fun setTryAgainClickListener(listener: OnClickListener) = binding.txtTryAgain.setOnClickListener(listener)
+
+}
